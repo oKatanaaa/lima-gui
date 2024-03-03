e5d76aa1b83657e9d8c5a70e1e5fd88ed1c6782f
oKatanaaa <whitemarstudios@gmail.com>
2024-03-03 16:13:05 +0400

    fix: segmentation fault crashes

--- lima_gui/app.py
+++ lima_gui/app.py
@@ -1,8 +1,10 @@
 from lima_gui.controller.main_controller import Controller
 from lima_gui.view.main_window import MainWindow
+import threading
 
 
 if __name__ == '__main__':
+    threading.stack_size(131072)  # 128KiB stack
     import sys
     from PySide6.QtWidgets import QApplication
 
